7f4df07c5b40845aec5a59fe60749a06b4d7c0d7
Максим-1987 <max-tenigin1987@mail.ru>
2020-09-28 18:26:18 +0300

    По нажатию на кнопку перейти в планировщик происходит переход на вкладку «Планировщик».

--- WpfTestMailSender/MainWindow.xaml.cs
+++ WpfTestMailSender/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Mail;
 using System.Windows;
+using System.Windows.Controls;
 using WpfTestMailSender.Models;
 
 namespace WpfTestMailSender
@@ -21,6 +22,12 @@ namespace WpfTestMailSender
         private void miClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void GoScheduler_Click(object sender, RoutedEventArgs e)
+        {
+            tc.SelectedIndex = 1;
+            
         }
     }
 }
