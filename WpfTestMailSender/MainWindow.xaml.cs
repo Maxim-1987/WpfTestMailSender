04528ff1f3b2d87f4fd2e2ec8803806867f4996b
Максим-1987 <max-tenigin1987@mail.ru>
2020-09-28 18:33:18 +0300

    добавлен код для кнопки отправить в планировщике

--- WpfTestMailSender/MainWindow.xaml.cs
+++ WpfTestMailSender/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Mail;
 using System.Windows;
 using System.Windows.Controls;
+using WpfTestMailSender.lib;
 using WpfTestMailSender.Models;
 
 namespace WpfTestMailSender
@@ -13,11 +14,35 @@ namespace WpfTestMailSender
         {
             InitializeComponent();
         }
+        private void OnSendButtonClick(object Sender, RoutedEventArgs E)
+        {
+            var sender = SendersList.SelectedItem as Sender;
+            if (sender is null) return;
 
-        //private void OnSendButtonClick(object sender, RoutedEventArgs e)
-        //{
-        //    EmailSendServiceClass.sendEmail(LoginEdit.Text, PasswordEdit.SecurePassword, Subject.Text, Text.Text);
-        //}
+            if (!(RecipientsList.SelectedItem is Recipient recipient)) return;
+            if (!(ServersList.SelectedItem is Server server)) return;
+            if (!(MessagesList.SelectedItem is Message message)) return;
+
+            var send_service = new EmailSendServiceClass
+            {
+                ServerAddress = server.Address,
+                ServerPort = server.Port,
+                UseSSL = server.UseSSL,
+                Login = server.Login,
+                Password = server.Password,
+            };
+
+            try
+            {
+                send_service.sendEmail(sender.fromAddress, recipient.toAddress, message.Subject, message.Text);
+            }
+            catch (SmtpException error)
+            {
+                MessageBox.Show(
+                    "Ошибка при отправке почты " + error.Message, "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
 
         private void miClose_Click(object sender, RoutedEventArgs e)
         {
