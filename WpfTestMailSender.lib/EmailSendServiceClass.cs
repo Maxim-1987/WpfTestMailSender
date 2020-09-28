66ebea495d60342521ff0d38326dd4082e2c9842
Максим-1987 <max-tenigin1987@mail.ru>
2020-09-28 17:59:15 +0300

    Добавлена библиотека с логикой программы

--- WpfTestMailSender.lib/EmailSendServiceClass.cs
+++ WpfTestMailSender.lib/EmailSendServiceClass.cs
@@ -1,41 +1,54 @@
-﻿using System;
+﻿using System.Diagnostics;
 using System.Net;
 using System.Net.Mail;
-using System.Windows;
-using System.Security;
 
 namespace WpfTestMailSender.lib
 {
-    class EmailSendServiceClass
+   public class EmailSendServiceClass
     {
-        EmailSendServiceClass()
+        public string ServerAddress { get; set; }
+
+        public int ServerPort { get; set; }
+
+        public bool UseSSL { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public void sendEmail(string SenderAddress, string RecipientAddress, string Subject, string Text)
         {
+            var from = new MailAddress(SenderAddress);
+            var to = new MailAddress(RecipientAddress);
 
+            using (var message = new MailMessage(from, to))
+            {
+
+                message.Subject = Subject;
+                message.Body = Text;
+
+                using (var client = new SmtpClient(ServerAddress, ServerPort))
+                {
+                    client.EnableSsl = UseSSL;
+
+                    client.Credentials = new NetworkCredential
+                    {
+                        UserName = Login,
+                        Password = Password
+                    };
+
+                    try
+                    {
+                        client.Send(message);
+                    }
+                    catch (SmtpException e)
+                    {
+                        Trace.TraceError(e.ToString());
+                        throw;
+                    }
+                }
+            }
         }
-
-        //public static void sendEmail(string LoginEdit, SecureString PasswordEdit, string Subject, string Text)
-        //{
-        //    var client = new SmtpClient(Variables.smtpServer, Variables.port);
-        //    client.EnableSsl = true;
-        //    client.Credentials = new NetworkCredential(LoginEdit, PasswordEdit);
-
-        //    var to = new MailAddress(Variables.toAddress, Variables.toName);
-        //    var from = new MailAddress(Variables.fromAddress, Variables.fromName);
-
-        //    var message = new MailMessage(from, to);
-
-        //    message.Subject =Subject + DateTime.Now;
-        //    message.Body = Text + DateTime.Now;
-
-        //    try
-        //    {
-        //        client.Send(message);
-        //        MessageBox.Show("Успех!");
-        //    }
-        //    catch (Exception error)
-        //    {
-        //        MessageBox.Show(error.Message, "Ошибка отправки");
-        //    }
-        //}
-    }
+   }
+   
 }
