9568b22587184c4fd7230a35356600035ac7124b
Максим-1987 <max-tenigin1987@mail.ru>
2020-10-05 16:11:09 +0300

    Добавлен пользовательский элемент управления Получатели

--- WpfTestMailSender/MainWindow.xaml.cs
+++ WpfTestMailSender/MainWindow.xaml.cs
@@ -13,46 +13,6 @@ namespace WpfTestMailSender
         public WpfMailSender()
         {
             InitializeComponent();
-        }
-        //private void OnSendButtonClick(object Sender, RoutedEventArgs E)
-        //{
-        //    var sender = SendersList.SelectedItem as Sender;
-        //    if (sender is null) return;
-
-        //    if (!(RecipientsList.SelectedItem is Recipient recipient)) return;
-        //    if (!(ServersList.SelectedItem is Server server)) return;
-        //    if (!(MessagesList.SelectedItem is Message message)) return;
-
-        //    var send_service = new EmailSendServiceClass
-        //    {
-        //        ServerAddress = server.Address,
-        //        ServerPort = server.Port,
-        //        UseSSL = server.UseSSL,
-        //        Login = server.Login,
-        //        Password = server.Password,
-        //    };
-
-        //    try
-        //    {
-        //        send_service.sendEmail(sender.FromAddress, recipient.ToAddress, message.Subject, message.Text);
-        //    }
-        //    catch (SmtpException error)
-        //    {
-        //        MessageBox.Show(
-        //            "Ошибка при отправке почты " + error.Message, "Ошибка",
-        //            MessageBoxButton.OK, MessageBoxImage.Error);
-        //    }
-        //}
-
-        //private void miClose_Click(object sender, RoutedEventArgs e)
-        //{
-        //    this.Close();
-        //}
-
-        //private void GoScheduler_Click(object sender, RoutedEventArgs e)
-        //{
-        //    tc.SelectedIndex = 1;
-            
-        //}
+        }        
     }
 }
